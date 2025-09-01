387e874b0216f2157b7c245247bd27d27b13862c
aleksey <av.pavlow@gmail.com>
2025-09-01 08:47:10 +0300

    chore: Добавить в исключения .gitignore __debug_bin*; feat: Проверить сгенерированные файлы .proto в main - __debug_bin* добавлены в исключения для git - Сгенерированные файлы проверены

--- cmd/orderservice/main.go
+++ cmd/orderservice/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-func main()  {
-	fmt.Println("Начало программы")
+	"google.golang.org/protobuf/proto"
+	pb "github.com/go-portfolio/order-pipeline/proto"
+)
+
+func main() {
+	// Создаём структуру OrderRequest
+	order := &pb.OrderRequest{
+		Id:    "123",
+		Item:  "Laptop",
+		Price: 1500,
+	}
+
+	// Сериализуем в protobuf-байты
+	data, err := proto.Marshal(order)
+	if err != nil {
+		log.Fatalf("Ошибка сериализации: %v", err)
+	}
+
+	fmt.Printf("Сериализованные байты: %x\n", data)
+
+	// Десериализуем обратно
+	var parsed pb.OrderRequest
+	if err := proto.Unmarshal(data, &parsed); err != nil {
+		log.Fatalf("Ошибка десериализации: %v", err)
+	}
+
+	// Проверка вывода
+	fmt.Printf("Распарсено: ID=%s, Item=%s, Price=%d\n", parsed.Id, parsed.Item, parsed.Price)
 }
