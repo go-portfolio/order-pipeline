2ae654e1a6c49405f991be9065e7600ef3216f7c
aleksey <av.pavlow@gmail.com>
2025-09-04 21:47:04 +0300

    fix: Исправить все ошибки с созданием контейнеров и передачей .env, сделать контейнера все запущенными; refact: Объединить скрипт sh для prod и debug - Все ошибки исправлены, контейнера запускаются - Скрипты объеденены в единый файл

---
+++ scripts/start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+# Проверяем, что SERVICE_BINARY задан
+if [ -z "$SERVICE_BINARY" ]; then
+  echo "Ошибка: переменная SERVICE_BINARY не задана!"
+  exit 1
+fi
+
+# Запуск
+if [ "$MODE" = "debug" ]; then
+  echo "Запуск $SERVICE_BINARY в режиме DEBUG"
+  # Для обычных сервисов
+  if [ "$SERVICE_BINARY" != "tests" ]; then
+    exec /dlv exec /app/$SERVICE_BINARY --headless --listen=:40000 --api-version=2 --accept-multiclient
+  else
+    # Для тестов используем dlv test
+    exec /dlv test ./... --headless --listen=:40000 --api-version=2 --accept-multiclient
+  fi
+else
+  echo "Запуск $SERVICE_BINARY в режиме PROD"
+  # Для тестов просто go test, для сервисов бинарник
+  if [ "$SERVICE_BINARY" != "tests" ]; then
+    exec /app/$SERVICE_BINARY
+  else
+    go test ./...
+  fi
+fi
